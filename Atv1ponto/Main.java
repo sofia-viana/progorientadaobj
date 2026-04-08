b0dc7381093b7abb1d1afd4e2a874b8319c78dc0
sofia-viana <sofiavianax9@gmail.com>
2026-04-08 21:15:30 +0000

    atv 1ponto

--- Atv1ponto/Main.java
+++ Atv1ponto/Main.java
@@ -23,13 +23,19 @@ public class Main {
         b.emprestar(1, 1, 1);
         b.emprestar(2, 2, 2);
         b.emprestar(3, 1, 3);
-        b.emprestar(4, 1, 1);
+        b.emprestar(4, 2, 4);
 
         Emprestimo e1 = b.emprestimos.get(0);
         e1.devolver(10);
         Emprestimo e2 = b.emprestimos.get(1);
         e2.devolver(0);
+        Emprestimo e3 = b.emprestimos.get(2);
+        e3.devolver(5);
+        Emprestimo e4 = b.emprestimos.get(3);
+        e4.devolver(16);
         System.out.println(e1.exibirResumo());
         System.out.println(e2.exibirResumo());
+        System.out.println(e3.exibirResumo());
+        System.out.println(e4.exibirResumo());
     }
 }
